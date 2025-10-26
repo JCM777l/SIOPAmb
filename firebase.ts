651abcb81610e995253a63c90ab0e8d036f5d4b3
JCM <jocemarnunes.rv@gmail.com>
2025-10-26 20:48:49 -0300

    feat: Update Firebase SDK version
    Upgrades the Firebase SDK to v12.4.0 to leverage new features and improvements. This includes updating the CDN links in index.html and adjusting the storageBucket configuration in firebase.ts.

--- firebase.ts
+++ firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB-53DNCV3PO496U_6fqYMnrr_moL8GgLQ",
   authDomain: "siopamb01.firebaseapp.com",
   projectId: "siopamb01",
-  storageBucket: "siopamb01.appspot.com",
+  storageBucket: "siopamb01.firebasestorage.app",
   messagingSenderId: "500057943461",
   appId: "1:500057943461:web:9fa495eb94838b79aa4c4c",
   measurementId: "G-KDE5YED75E"
